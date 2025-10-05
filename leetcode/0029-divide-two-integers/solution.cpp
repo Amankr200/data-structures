11fb7e763c27dc7bf60090ef3716141cff50ac16
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2025-10-05 04:22:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (38.03%)

---
+++ leetcode/0029-divide-two-integers/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        long long a = labs(dividend), b = labs(divisor), res = 0;
+        while (a >= b) {
+            long long temp = b, multiple = 1;
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            a -= temp;
+            res += multiple;
+        }
+        if ((dividend < 0) ^ (divisor < 0)) res = -res;
+        return res;
+    }
+};
+
