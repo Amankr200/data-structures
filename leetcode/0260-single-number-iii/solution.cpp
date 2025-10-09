133fbce8a21fbd17a1bb66ce4ded4303905797f7
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2025-10-09 09:35:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.9 MB (58.89%)

---
+++ leetcode/0260-single-number-iii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+   vector<int> singleNumber(vector<int>& nums) {
+    long long xorAll = 0;
+    for (int num : nums)
+        xorAll ^= num;
+ 
+    long long diffBit = xorAll & -xorAll;
+
+    int a = 0, b = 0;
+    for (int num : nums) {
+        if (num & diffBit)
+            a ^= num;
+        else
+            b ^= num;
+    }
+    return {a, b};
+}
+};
