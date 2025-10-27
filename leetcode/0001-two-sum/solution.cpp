98fe611a982bb44a95fafac8973a9614992b85cd
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2025-10-27 18:18:45 +0000

    [LeetCode Sync] Runtime - 4 ms (54.30%), Memory - 15 MB (13.03%)

--- leetcode/0001-two-sum/solution.cpp
+++ leetcode/0001-two-sum/solution.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {  
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};  
-                }
+        map<int, int> mp; // key = number, value = index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            // check if complement exists in map
+            if (mp.find(complement) != mp.end()) {
+                // found pair: return their indices
+                return {mp[complement], i};
             }
+
+            // otherwise, store current element with index
+            mp[nums[i]] = i;
         }
-        return {}; 
+
+        return {}; // no solution
     }
 };
 
