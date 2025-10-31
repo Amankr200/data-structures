760592768096ca5c9f65fd294e44e9f2ea68433b
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2025-10-31 05:37:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26.1 MB (51.90%)

---
+++ leetcode/3581-the-two-sneaky-numbers-of-digitville/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n, 0);
+        vector<int> ans;
+        
+        for (int num : nums) {
+            freq[num]++;
+            if (freq[num] == 2) ans.push_back(num);
+        }
+        return ans;
+    }
+};
+
