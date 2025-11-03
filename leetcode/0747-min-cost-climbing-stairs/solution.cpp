5d127f19d799769c3ad6e53fb96b28a76d58d3d4
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2025-11-03 16:33:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (29.46%)

--- leetcode/0747-min-cost-climbing-stairs/solution.cpp
+++ leetcode/0747-min-cost-climbing-stairs/solution.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& cost, vector<int>& dp) {
-        if (i < 0) return 0;
-        if (i == 0 || i == 1) return cost[i];
-        if (dp[i] != -1) return dp[i];
-        return dp[i] = cost[i] + min(solve(i - 1, cost, dp), solve(i - 2, cost, dp));
+    int recursion(int i, vector<int>& cost, vector<int>& dp) {
+        if (i >= cost.size()) return 0;  // base case
+        if (dp[i] != -1) return dp[i];   // already computed
+        
+        int oneStep = recursion(i + 1, cost, dp);
+        int twoStep = recursion(i + 2, cost, dp);
+        
+        dp[i] = cost[i] + min(oneStep, twoStep);
+        return dp[i];
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n, -1);
-        return min(solve(n - 1, cost, dp), solve(n - 2, cost, dp));
+        vector<int> dp(n, -1);  // initialize dp with -1
+        return min(recursion(0, cost, dp), recursion(1, cost, dp));
     }
 };
 
