6a01ac539e054ca1185a280b40708aab537c502a
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2025-12-03 09:28:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.3 MB (99.34%)

---
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return {first, last};
+    }
+
+private:
+    int findFirst(vector<int>& nums, int target) {
+        int ans = -1;
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;   // move left
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    int findLast(vector<int>& nums, int target) {
+        int ans = -1;
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;   // move right
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
