fcca34485d7fe06a2cd652bd833b6aeafa6dd929
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2026-02-12 04:52:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.7 MB (31.17%)

--- leetcode/0547-number-of-provinces/solution.cpp
+++ leetcode/0547-number-of-provinces/solution.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<bool> visited(n, false);
-        int count = 0;
+ 
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                count++;    
+int findCircleNum(vector<vector<int>>& isConnected) {
+    int n = isConnected.size();
+    vector<vector<int>> adj(n);
 
-                queue<int> q;
-                q.push(i);
-                visited[i] = true;
+    // matrix → adjacency list
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (isConnected[i][j] == 1 && i != j) {
+                adj[i].push_back(j);
+            }
+        }
+    }
 
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
+    vector<bool> visited(n, false);
+    int provinces = 0;
 
-                    for (int j = 0; j < n; j++) {   
-                        if (isConnected[node][j] == 1 && !visited[j]) {
-                            visited[j] = true;
-                            q.push(j);
-                        }
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            provinces++;
+            queue<int> q;
+            q.push(i);
+            visited[i] = true;
+
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+
+                for (int neigh : adj[node]) {
+                    if (!visited[neigh]) {
+                        visited[neigh] = true;
+                        q.push(neigh);
                     }
                 }
             }
         }
-        return count;
     }
-};
+    return provinces;
+}
 
+
+};
