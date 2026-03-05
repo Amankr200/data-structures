19f70e42ff469ff5962715457f71f6237736ee54
Amankr200 <160874720+Amankr200@users.noreply.github.com>
2026-03-05 13:21:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26.3 MB (34.40%)

---
+++ leetcode/0540-single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+  int singleNonDuplicate(vector<int>& arr) {
+
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low < high) {
+
+        int mid = (low + high) / 2;
+
+        // make mid even
+        if (mid % 2 == 1)
+            mid--;
+
+        if (arr[mid] == arr[mid + 1])
+            low = mid + 2;
+        else
+            high = mid;
+    }
+
+    return arr[low];
+}
+};
